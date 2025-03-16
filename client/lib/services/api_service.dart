bf2a87599cf9fda1d5004432a7e2e71105e95c42
analyticallyblessed <ashis1988h@gmail.com>
2025-03-17 04:55:36 +0530

    Update api_service.dart

--- client/lib/services/api_service.dart
+++ client/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const String baseUrl = 'http://localhost:8080';
+  static const String baseUrl = 'https://dart_server-a060-nctk0jj-analyticallyblessed.globeapp.dev/';
 
   Future<String> login(String username, String password) async {
     final response = await http.post(
