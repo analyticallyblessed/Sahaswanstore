98a332f90d6950d1dbd7424247a968a61ec7f60b
analyticallyblessed <ashis1988h@gmail.com>
2025-03-17 04:23:47 +0530

    Create lib/main.dart

---
+++ client/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'presentation/providers/auth_provider.dart';
+import 'presentation/pages/login_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AuthProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: LoginPage(),
+    );
+  }
+}
